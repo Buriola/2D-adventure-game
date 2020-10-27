54dfb1082dc6c1f4d71298e4fdb2700e432bcb91
Igor Buriola <igor.buriola@aquiris.com.br>
2020-10-27 18:59:16 -0300

    platform waypoints

--- Assets/Scripts/Buriola/Gameplay/Platforms/PlatformEntity2D.cs
+++ Assets/Scripts/Buriola/Gameplay/Platforms/PlatformEntity2D.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Buriola._2D_Physics;
 using UnityEngine;
 
@@ -8,15 +9,35 @@ namespace Buriola.Gameplay.Platforms
     public sealed class PlatformEntity2D : BaseEntity2D
     {
         [SerializeField] private LayerMask _passengerMask = default;
-        [SerializeField] private Vector2 _moveAmount = Vector2.zero;
+        [SerializeField] private bool _isCyclic = false;
+        [SerializeField, Range(0f, 20f)] private float _speed = 1f;
+        [SerializeField, Range(0f, 2f)] private float _easeAmount = 0f;
+        [SerializeField, Range(0f, 5f)] private float _waitTime = 1f;
+        [SerializeField] private Vector2[] _localWaypoints = null;
         
         private readonly Dictionary<string, Entity2D> _passengers = new Dictionary<string, Entity2D>();
         private readonly List<PassengerMovement> _passengerMovements = new List<PassengerMovement>();
         private readonly HashSet<Transform> _movedPassengers = new HashSet<Transform>();
 
+        private int _fromWaypointIndex;
+        private float _percentBetweenWaypoints;
+        private float _nextMoveTime;
+        
+        private Vector2[] _globalWaypoints = null;
         private Vector2 _velocity = Vector2.zero;
         private Vector2 _velocityVertically = Vector2.zero;
         private Vector2 _velocityHorizontally = Vector2.zero;
+
+        protected override void Start()
+        {
+            base.Start();
+            
+            _globalWaypoints = new Vector2[_localWaypoints.Length];
+            for (int i = 0; i < _localWaypoints.Length; i++)
+            {
+                _globalWaypoints[i] = _localWaypoints[i] + (Vector2)transform.position;
+            }
+        }
 
         protected override void Update()
         {
@@ -24,7 +45,7 @@ namespace Buriola.Gameplay.Platforms
             
             UpdateRaycastOrigins();
             
-            _velocity = _moveAmount * DeltaTime;
+            _velocity = CalculatePlatformMovement();
             
             CalculatePassengerMovement(_velocity);
             
@@ -33,6 +54,48 @@ namespace Buriola.Gameplay.Platforms
             MovePassengers(false);
         }
 
+        private Vector2 CalculatePlatformMovement()
+        {
+            if(Time.time < _nextMoveTime) return Vector2.zero;
+            
+            _fromWaypointIndex %= _globalWaypoints.Length;
+            
+            int toWaypointIndex = (_fromWaypointIndex + 1) % _globalWaypoints.Length;
+            float distanceBetweenWaypoints = Vector2.Distance(_globalWaypoints[_fromWaypointIndex], _globalWaypoints[toWaypointIndex]);
+
+            _percentBetweenWaypoints += DeltaTime * _speed / distanceBetweenWaypoints;
+            _percentBetweenWaypoints = Mathf.Clamp01(_percentBetweenWaypoints);
+
+            float easedPercent = Ease(_percentBetweenWaypoints);
+            
+            Vector2 newPosition = Vector2.Lerp(_globalWaypoints[_fromWaypointIndex], _globalWaypoints[toWaypointIndex], easedPercent);
+
+            if (_percentBetweenWaypoints >= 1f)
+            {
+                _percentBetweenWaypoints = 0f;
+                _fromWaypointIndex++;
+
+                if (!_isCyclic)
+                {
+                    if (_fromWaypointIndex >= _globalWaypoints.Length - 1)
+                    {
+                        _fromWaypointIndex = 0;
+                        Array.Reverse(_globalWaypoints);
+                    }
+                }
+
+                _nextMoveTime = Time.time + _waitTime;
+            }
+            
+            return newPosition - (Vector2) transform.position;
+        }
+
+        private float Ease(float x)
+        {
+            float a = _easeAmount + 1;
+            return Mathf.Pow(x, a) / (Mathf.Pow(x, a) + Mathf.Pow(1 - x, a));
+        }
+        
         private void MovePassengers(bool beforeMovePlatform)
         {
             foreach (PassengerMovement movement in _passengerMovements)
@@ -173,6 +236,25 @@ namespace Buriola.Gameplay.Platforms
                             _passengerMovements.Add(movement);
                         }
                     }
+                }
+            }
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (_localWaypoints != null)
+            {
+                Gizmos.color = Color.yellow;
+                float size = .3f;
+
+                for (int i = 0; i < _localWaypoints.Length; i++)
+                {
+                    bool isPlaying = Application.isPlaying;
+                    Vector2 globalWaypointPos = isPlaying ? _globalWaypoints[i] : _localWaypoints[i] + (Vector2)transform.position;
+                    
+                    Gizmos.DrawSphere(globalWaypointPos, .15f);
+                    Gizmos.DrawLine(globalWaypointPos - Vector2.up * size, globalWaypointPos + Vector2.up * size);
+                    Gizmos.DrawLine(globalWaypointPos - Vector2.left * size, globalWaypointPos + Vector2.left * size);
                 }
             }
         }
